4e3a7c3b95a08d237f58d2243186ac6d96ab16ad
EAR19000614 - Joseph Early <EAR19000614@wakecoll.net>
2023-03-20 10:52:09 +0000

    Expose to Lua and Lua Interaction scripts

---
+++ Assets/Scripts/LuaInteraction.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoonSharp.Interpreter;
+using System;
+
+namespace Lua
+{
+    public class LuaInteraction : MonoBehaviour
+    {
+        State state = State.ExecutionNone;
+        Transform trans;
+        Script script = new Script();
+
+        string TemplateScript = @"
+        function Tick()
+            // Any code here is called every frame of the game while the script is executing
+        end
+
+        function ExecutionStart()
+            // Any code here is called only when the script is first executed
+        end
+
+        function Reset()
+            // Called when Terminate(true) is executed to reset all properties
+        end
+        ";
+
+        // Reference to transform
+        private void Awake() => trans = GetComponent<Transform>();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (state == State.ExecutionNone) return;
+
+            // Do script
+            script.DoString(TemplateScript);
+
+            if (state == State.ExecutionStart)
+                script.Call(script.Globals["ExecutionStart"]);
+            else
+                script.Call(script.Globals["Tick"]);
+        }
+
+        internal void Expose(bool ExposeMovement, bool ExposeEnabled, bool ExposeColours, bool ExposeRotation, bool ExposeScale, bool ReadOnly)
+        {
+            script.Globals["Terminate"] = (Func<bool>)Terminate; // Terminate script
+
+            // Expose movement functionality
+            if (ExposeMovement)
+            {
+                script.Globals["Move"] = (Func<float, float, int>)Move; // Basic movement method
+            }
+        }
+
+        #region Exposed functions
+        private static int Move(float x, float y)
+        {
+            var trans = GetComponent<Transform>();
+
+            var pos = new UnityEngine.Vector2(trans.position.x + x, trans.position.y + y);
+
+            trans.position = pos;
+
+            return 0;
+        }
+
+        private static bool Terminate(bool reset)
+        {
+            // Reset everything here
+            script.Call(script.Globals["Tick"]);
+
+            // Terminate script
+            state = State.ExecutionNone;
+        }
+        #endregion
+    }
+
+    // State
+    enum State
+    {
+        ExecutionStart,
+        ExecutionTick,
+        ExecutionNone
+    }
+
+}
